e1f70af110add0537d0ecdf131105aeee5950196
frgarcia <francisco.garcia.ralph@gmail.com>
2016-05-11 15:51:15 -0300

    Primer commit de la maqueta v 1.0

---
+++ resultados.php
@@ -0,0 +1,320 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+	<title>Lorem Ipsum</title>
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:600italic,400,800,700,300' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=BenchNine:300,400,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300">
+    <link rel="stylesheet" href="https://cdn.rawgit.com/yahoo/pure-release/v0.6.0/pure-min.css">
+   
+    <link rel="stylesheet" href="auto-complete.css">
+	<script src="js/modernizr.js"></script>
+
+
+	<!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+	
+	<!-- ====================================================
+	header section -->
+	<header class="top-header">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-3 header-logo">
+
+					<a href="index.html"><img src="img/logo.png" alt="" class="img-responsive logo"></a>
+				</div>
+
+				<div class="col-md-9">
+					<nav class="navbar navbar-default">
+					  <div class="container-fluid nav-bar">
+					    <!-- Brand and toggle get grouped for better mobile display -->
+					    <div class="navbar-header">
+					      <button type="button" class="navbar-toggle collapsed " data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					        <span class="sr-only">Toggle navigation</span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					        <span class="icon-bar"></span>
+					      </button>
+					    </div>
+
+					    <!-- Collect the nav links, forms, and other content for toggling -->
+					    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					      
+					      <ul class="nav navbar-nav navbar-right">
+					        <li><a class="menu active" href="#home" >Inicio</a></li>
+					        <li><a class="menu" href="#buscar">Buscar</a></li>
+					        <li><a class="menu" href="#nosotros">Sobre nosotros</a></li>
+					        <li><a class="menu" href="#profesor">Profesores</a></li>
+					        <li><a class="menu" href="#corrector">¿Quieres ser corrector? </a></li>
+					        <li><a class="menu" href="http://ciae.edocere.com/login/index.php">Entrar</a></li>
+					        
+					        
+					      </ul>
+					    </div><!-- /navbar-collapse -->
+					  </div><!-- / .container-fluid -->
+					</nav>
+				</div>
+			</div>
+		</div>
+	</header> <!-- end of header area -->
+
+	
+<!-- buscar section -->
+	<section class="about text-center" id="buscar">
+		<div class="container">
+			<div class="row">
+				<h2></h2>
+				<div class="col-md-12 col-sm-6">
+					<form  method="get" action="resultados.php" class="pure-form" style="border-top: 1px solid #eee;border-bottom:1px solid #eee;background:#fafafa;padding:20px 10px;text-align:center"> 
+					<div class="single-about-detail clearfix">
+						
+						<div class="">
+						
+							<div class="form-group">
+  								<div class="col-xs-3">
+  									
+  									
+       								 <input id="tipo" autofocus type="text" name="type" placeholder="Tipo" style="width:100%;max-width:600px" >
+       							</div>	 
+								<div class="col-xs-7">
+  									<input id="recursos" autofocus type="text" name="name" placeholder="Nombre" style="width:100%;max-width:600px">
+  								</div>
+  								<div class="col-xs-2">
+  									 <button type="submit" class="btn btn-default">Entrar</button>
+  									
+  								</div>
+							</div>
+						</div>
+					</div>
+					</form>
+
+				</div>
+					<nav class="navbar navbar-default navbar-static-top" role="navigation">
+						 <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Tareas</a></li>
+      <li><a href="#">Rúbricas</a></li>
+      <li><a href="#">Marcadores</a></li>
+
+					</nav>
+				<div class="row">
+				
+				<div class="col-md-4 col-sm-6">
+					<div class="single-result-detail clearfix">
+
+						<div class="result-details" data-toggle="modal" data-target="#myModal">
+						
+							<h3>Amigos por correspondencia</h3>
+							<p style="text-align: left">Autor: Natalia Ávila</p>
+							<p style="text-align: left">Género: Carta de presentación personal</p>
+							<p style="text-align: left">Audiencia: Amigo</p>
+							<p style="text-align: left">Duración: 90 min.</p>
+							<p style="text-align: left">Contiene: <span class="glyphicon glyphicon-film" aria-hidden="false"></span> <span class="glyphicon glyphicon-book" aria-hidden="false"></span> <span class="glyphicon glyphicon-print" aria-hidden="false"></span></p>
+							<p style="text-align: left">El producto final es una carta de dos párrafos de extensión destinada a un amigo de un curso paralelo, de un colegio cercano o de séptimo año, según resulte más adecuado a la realidad del colegio</p>
+						    <div class="col-md-4 col-sm-6">
+								<p style="text-align: center"> 500 Visitas</p>
+						    </div>
+						    <div class="col-md-4 col-sm-6">
+								<p style="text-align: center"> 15 Comentarios </p>
+						    </div>
+						    <div class="col-md-4 col-sm-6">
+								<p style="text-align: center"> 250 Votos</p>
+						    </div>
+
+				
+							<p style="text-align: center"> <span class="glyphicon glyphicon-star" aria-hidden="true" style="font-size: 24px;"></span>
+								<span class="glyphicon glyphicon-star" aria-hidden="true"style="font-size: 24px;"></span>
+								<span class="glyphicon glyphicon-star" aria-hidden="true"style="font-size: 24px;"></span>
+								<span class="glyphicon glyphicon-star" aria-hidden="true"style="font-size: 24px;"></span>
+								<span class="glyphicon glyphicon-star-empty" aria-hidden="true"style="font-size: 24px;"></span></p>
+						
+						
+						</div>
+					</div>
+				</div>
+				<div class="row">
+				
+				<div class="col-md-4 col-sm-6">
+					<div class="single-result-detail clearfix">
+
+						<div class="result-details" data-toggle="modal" data-target="#myModal">
+						
+							<h3>Los Mayas</h3>
+							<p style="text-align: left">Autor: Agencia de Calidad de la Educación </p>
+							<p style="text-align: left">Género: Carta de presentación personal</p>
+							<p style="text-align: left">Duración: 45 min.</p>
+							<p style="text-align: left">Contiene:  <span class="glyphicon glyphicon-book" aria-hidden="false"></span> </p>
+							<p style="text-align: left">El alumno debe escribir un articulo con el fín de informar a sus compañeros.</p>
+						    <div class="col-md-4 col-sm-6">
+								<p style="text-align: center"> 250 Visitas</p>
+						    </div>
+						    <div class="col-md-4 col-sm-6">
+								<p style="text-align: center"> 7 Comentarios </p>
+						    </div>
+						    <div class="col-md-4 col-sm-6">
+								<p style="text-align: center"> 116 Votos</p>
+						    </div>
+
+				
+							<p style="text-align: center"> <span class="glyphicon glyphicon-star" aria-hidden="true" style="font-size: 24px;"></span>
+								<span class="glyphicon glyphicon-star" aria-hidden="true"style="font-size: 24px;"></span>
+								<span class="glyphicon glyphicon-star" aria-hidden="true"style="font-size: 24px;"></span>
+								<span class="glyphicon glyphicon-star-empty" aria-hidden="true"style="font-size: 24px;"></span>
+								<span class="glyphicon glyphicon-star-empty" aria-hidden="true"style="font-size: 24px;"></span></p>
+						
+						
+						</div>
+					</div>
+				</div>
+				<div class="row">
+				
+				<div class="col-md-4 col-sm-6">
+					<div class="single-result-detail clearfix">
+
+						<div class="result-details" data-toggle="modal" data-target="#myModal">
+						
+							<h3>Uso de comas</h3>
+							<p style="text-align: left">Autor: Francisco García</p>
+							<p style="text-align: left">Género: Gramática</p>
+							<p style="text-align: left">Duración: 30 min.</p>
+							<p style="text-align: left">Actividad para alumnos de 6to básico, sobre el uso de las comas.</p>
+						    <div class="col-md-4 col-sm-6">
+								<p style="text-align: center"> 98 Visitas</p>
+						    </div>
+						    <div class="col-md-4 col-sm-6">
+								<p style="text-align: center"> 2 Comentarios </p>
+						    </div>
+						    <div class="col-md-4 col-sm-6">
+								<p style="text-align: center"> 13 <br>Votos </p>
+						    </div>
+
+							
+							<p style="text-align: center"> <span class="glyphicon glyphicon-star" aria-hidden="true" style="font-size: 24px;"></span>
+								<span class="glyphicon glyphicon-star-empty" aria-hidden="true"style="font-size: 24px;"></span>
+								<span class="glyphicon glyphicon-star-empty" aria-hidden="true"style="font-size: 24px;"></span>
+								<span class="glyphicon glyphicon-star-empty" aria-hidden="true"style="font-size: 24px;"></span>
+								<span class="glyphicon glyphicon-star-empty" aria-hidden="true"style="font-size: 24px;"></span></p>
+							
+						
+						</div>
+					</div>
+				</div>
+
+
+
+
+
+			</div>
+		</div>
+	</section><!-- end of buscar section -->
+
+	<!-- footer starts here -->
+	<footer class="footer clearfix">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-6 footer-para">
+					<p>&copy; All right reserved</p>
+
+				</div>
+				<div class="col-xs-6 text-right">
+					<a href=""><i class="fa fa-facebook"></i></a>
+					<a href=""><i class="fa fa-twitter"></i></a>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog modal-lg">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div>
+      <div class="modal-body">
+        <h2>Amigos por correspondencia</h2>
+        <div class="alert alert-danger" role="alert">
+  		<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+  		<span class="sr-only">Error:</span>  Debes <a href="#">iniciar sesión</a> para descargar el contenido.  Si no tienes cuenta <a href="http://localhost:8080/moodle/login/signup.php">registrate gratis.</a>
+		</div>
+       
+				
+				<h3>Ejemplos</h3>
+        		<p style="text-align: right">Quito, 3 de noviembre de 2016</p>
+        		<p style="text-align: left">Estimada amiga:</p>
+        		<p style="text-align: left">Hola, Nati. Espero que te encuentres bien al recibir esta primera carta. Estoy muy contento de que seamos amigos por correspondencia, es algo muy especial ya que las cartas casi no se usan ahora.
+Te cuento que vivo en Ecuador y me encanta jugar béisbol. Pero además, me gusta mucho el animé y sueño con tener mi propio “fan fiction” con una historia paralela de Naruto. ¿Conoces la animación japonesa? ¿Has visto Naruto alguna vez?</p>
+				<p style="text-align: left">Soy un niño normal, tengo pocos amigos pero muy cercanos. En el colegio me va más o menos, pero mis clases favoritas son dibujo y lenguaje. 
+Espero tu carta para saber cómo eres tú. También te mando una foto de Quito, la ciudad en que vivo.</p>
+				<p style="text-align: left">Muchos cariños y hasta pronto,</p>
+				<p style="text-align: left">Juan Anabalón</p>
+				<h3>Videos</h3>
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/_LfywvezuVQ" frameborder="0" allowfullscreen></iframe>
+				<div  id="comentarios">
+				<h3>Comentarios</h3>
+				</div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+	<!-- script tags
+	============================================================= -->
+	<script src="js/jquery-2.1.1.js"></script>
+	
+	<script src="js/smoothscroll.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/custom.js"></script>
+	<script src="js/auto-complete.js"></script>
+    <script>
+        var tipo = new autoComplete({
+            selector: '#tipo',
+            minChars: 1,
+            source: function(term, suggest){
+                term = term.toLowerCase();
+                var choices = ['Marcadores','Rúbricas','Tareas'];
+                var suggestions = [];
+                for (i=0;i<choices.length;i++)
+                    if (~choices[i].toLowerCase().indexOf(term)) suggestions.push(choices[i]);
+                suggest(suggestions);
+            }
+        });
+         var recursos = new autoComplete({
+            selector: '#recursos',
+            minChars: 1,
+            source: function(term, suggest){
+                term = term.toLowerCase();
+                var choices = ['Los Mayas','Acto fin de año', 'La contaminación','Día internacional de la Mujer','LE05 OA 01','LE05 OA 02','LE05 OA 03','LE05 OA 05','LE05 OA 06','El tren','Propósito comunicativo: Informar','Propósito comunicativo: Opinar','Propósito comunicativo: Narrar','Cuento', 'Carta', 'Noticia'];
+                var suggestions = [];
+                for (i=0;i<choices.length;i++)
+                    if (~choices[i].toLowerCase().indexOf(term)) suggestions.push(choices[i]);
+                suggest(suggestions);
+            }
+
+        });
+
+    </script>
+    <script src="https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js" async defer></script>
+   
+</body>
+</html>
