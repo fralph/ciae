a4ea00bc7c2ecce6512e8579024cdabc6b6464ec
frgarcia <francisco.garcia.ralph@gmail.com>
2016-09-13 13:17:44 -0300

    cambios en el buscador de actividades

--- buscar.php
+++ buscar.php
@@ -53,7 +53,7 @@ if (isloggedin ()) {
 <!-- Fin Script Javascript -->
 <!-- Scripts JQuery -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-<script type="text/javascript" src="jquery-1.8.0.min.js"></script> 
+
     <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
     <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
 <!-- Script para filtro de genero -->
@@ -74,6 +74,7 @@ $(document).ready(function () {
         $('#genero').show().siblings('#loadMore').show();
         $('#genero').show().siblings('#showLess').hide();
     });
+   
 });
 </script>
 <!-- Script del slider para filtro de Tiempo estimadoión -->
@@ -105,204 +106,15 @@ $(function() {
 <!-- fIN DEL header -->
 
 <!-- BUSCADOR -->
-<section class="buscar text-center">
-	<div class="container">
-		<div class="row">
-		<h3></h3>
-		<h3></h3>
-			<h2>Buscar actividad</h2>
-			<div class="col-md-1 col-sm-0">
-			</div>
-			<div class="col-md-11">
-				<form method="get" action="resultados.php" class="pure-form">
-					<div class="form-group">
-						<div class="col-md-7">
-							<input class="recursos"  type="text" name="name" placeholder="Nombre">
-						</div>
-						<div class="col-md-3">
-							<button type="submit" class="btn btn-default btn-lg">Buscar</button>
-						</div>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-</section>
+<?php include_once 'forms/search.php';?>
 <!-- FIN BUSCADOR -->
 
 <!-- Barra de información sobre el resultado, incluye un ordenador del mismo -->
-<section class="barraResultado">
-<hr>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-1">
-			</div>
-			<div class="col-md-8">
-				<p>3 Resultado(s) </p>
-				
-			</div>
-			<div class="col-md-2">
-				<select class="form-control">
-					<option>Ordenar por:</option>
- 					<option>Comentarios</option>
-  					<option>Likes</option>
-  					<option>Vistos</option>
-				</select>
-			</div>
-		</div>
-	</div>
-</section>
+
 <!-- FIN BARRA DE INFORMACIÓN DE RESULTADO -->	
 
 <!-- RESULTADOS -->
-<section class="resultados">
-	<div class="container">
-		<div id="filtros" class="col-md-3" style="text-align: left">
-			<div class="panel panel-default">
-				<div class="panel-body">	
-					<h1 style="text-align: left">Filtros</h1>
-					<h3 style="text-align: left">Tipo de recurso</h3>
-					<ul>
-						<li>
-						<div class="checkbox">
-						<input type="checkbox" name="rubrica" value="1"> Actividad
-						</div>
-						</li>
-						<li>
-						<div class="checkbox">
-						<input type="checkbox" name="rubrica" value="1"> Rúbrica
-						</div>
-						</li>
-					</ul>
-					<h3 style="text-align: left">Géneros</h3>
-					<ul id="genero">
-						<?php
-						// recorre el arreglo que contiene todos los generos, por cada uno crea un checkbox y una lista
-						for($i=0;$i<count($generos);$i++){
-							echo'<li>
-							<div class="checkbox">
-							<input type="checkbox" name="'.$generos[$i].'"> '.$generos[$i].'
-							</div>
-							</li>';
-						}
-						?>
-					</ul>
-					<div id="loadMore">Ver más...</div>
-					<div id="showLess">Ver menos...</div>	
-					<h3 style="text-align: left"></h3>
-					
-					<h3 style="text-align: left">Calificación</h3>
-					<ul id="calificacion">
-						<li>
-						<div class="checkbox">
-							<input type="checkbox" name="unaEstrella" value="1">
-							<span class="glyphicon glyphicon-star" aria-hidden="true" ></span>
-							<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
-							<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
-							<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
-							<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
-						</div>
-						</li>
-						<li>
-						<div class="checkbox">
-							<input type="checkbox" name="dosEstrella" value="1">
-							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-							<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
-							<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
-							<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
-						</div>
-						</li>
-						<li>
-						<div class="checkbox">
-							<input type="checkbox" name="tresEstrella" value="1">
-							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-							<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
-							<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
-						</div>
-						</li>
-						<li>
-						<div class="checkbox">
-							<input type="checkbox" name="cuatroEstrella" value="1">
-							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-							<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
-						</div>
-						</li>
-						<li>
-						<div class="checkbox">
-							<input type="checkbox" name="cincoEstrella" value="1">
-							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-						</div>
-						</li>
-					</ul>					
-				</div>
-			</div>
-		</div>
-		<div id="resultados" class="col-md-9" style="text-align: left">
-			
-			<div class="panel panel-default">
-			<div class="single-result-detail clearfix">
-			<div data-toggle="modal" data-target="#myModal">
-			
-				<div id="descripcion" class="panel-body">
-			<center><h3>Amigos por correspondencia</h3></center>
-					<div  class="col-md-4" style="text-align: left">
-						<p>Propósito Comunicativo: Informar</p>
-						<p>Género: Carta de presentación personal</p>
-						<p>Audiencia: Amigo</p>
-						<p>Tiempo estimado: 90 min.</p>
-						<p>Contiene: 
-							<span class="glyphicon glyphicon-film" aria-hidden="false"></span>
-							<span class="glyphicon glyphicon-book" aria-hidden="false"></span>
-							<span class="glyphicon glyphicon-print" aria-hidden="false"></span>
-						</p>
-					</div>
-					<div  class="col-md-5">
-						<p>El producto final es una carta
-							de dos párrafos de extensión destinada a un amigo de un
-							curso paralelo, de un colegio cercano o de séptimo año,
-							según resulte más adecuado a la realidad del colegio El
-							producto final es una carta de dos párrafos de extensión
-							destinada a un amigo de un curso paralelo, de un colegio
-							cercano o de séptimo año, según resulte más adecuado a la
-							realidad del colegio</p>
-					</div>
-					<div  class="col-md-3" style="text-align: left">
-					<img src="img/premio.png" class="premio" height="40px" width="40px">
-					<p>55 Visitas</p>
-					<p>3 Comentarios</p>
-					<p>20 votos</p>
-					<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-					<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-					<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-					<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-					<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
-					<p></p><p></p>
-					<p style="font-size: 14px;">Creado por:<a href="perfil.html"> Natalia Ávila</a></p>
-					
-					</div>
-				</div>
-			</div>	
-			</div>
-			</div>
-			
-			
-			</div>	
-			</div>
-			</div>
-			
-		</div>
-	</div>
-</section>
+<?php include_once 'resultados.php';?>
 <!-- FIN RESULTADOS -->	
 </body>
 <!-- footer starts here -->
--- resultados.php
+++ resultados.php
@@ -1,226 +1,169 @@
-<?php
-require_once (dirname ( dirname ( dirname ( __FILE__ ) ) ) . '/config.php');
-GLOBAL $USER, $CFG;
-$teacherroleid = 3;
-$logged = false;
-
-if (isloggedin ()) {
-	$logged = true;
-	$courses = enrol_get_all_users_courses ( $USER->id );
-	$countcourses = count ( $courses );
-	foreach ( $courses as $course ) {
-		$context = context_course::instance ( $course->id );
-		$roles = get_user_roles ( $context, $USER->id, true );
-		foreach ( $roles as $rol ) {
-			if ($rol->roleid == $teacherroleid) {
-				$asteachercourses [$course->id] = $course->fullname;
-			}
-		}
-	}
-}
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="">
-<meta name="author" content="">
-<title>Lorem Ipsum</title>
-<link rel="stylesheet" href="css/font-awesome.min.css">
-<link rel="stylesheet" href="css/bootstrap.min.css">
-<link rel="stylesheet" href="css/style.css">
-<link
-	href='http://fonts.googleapis.com/css?family=Open+Sans:600italic,400,800,700,300'
-	rel='stylesheet' type='text/css'>
-<link
-	href='http://fonts.googleapis.com/css?family=BenchNine:300,400,700'
-	rel='stylesheet' type='text/css'>
-<link rel="stylesheet"
-	href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300">
-<link rel="stylesheet"
-	href="https://cdn.rawgit.com/yahoo/pure-release/v0.6.0/pure-min.css">
-
-<link rel="stylesheet" href="auto-complete.css">
-<script src="js/modernizr.js"></script>
-
-
-<!--[if lt IE 9]>
-      <script src="js/html5shiv.js"></script>
-      <script src="js/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-<body>
-
-	<!-- ====================================================
-	header section -->
-	<header class="top-header">
-		<div class="container">
-			<div class="row">
-				<div class="col-xs-3 header-logo">
-
-					<a href="index.html"><img src="img/logo.png" alt=""
-						class="img-responsive logo"></a>
-				</div>
-
-				<div class="col-md-9">
-					<nav class="navbar navbar-default">
-						<div class="container-fluid nav-bar">
-							<!-- Brand and toggle get grouped for better mobile display -->
-							<div class="navbar-header">
-								<button type="button" class="navbar-toggle collapsed "
-									data-toggle="collapse"
-									data-target="#bs-example-navbar-collapse-1">
-									<span class="sr-only">Toggle navigation</span> <span
-										class="icon-bar"></span> <span class="icon-bar"></span> <span
-										class="icon-bar"></span>
-								</button>
-							</div>
-
-							<!-- Collect the nav links, forms, and other content for toggling -->
-							<div class="collapse navbar-collapse"
-								id="bs-example-navbar-collapse-1">
-
-								<ul class="nav navbar-nav navbar-right">
-									<li><a class="menu active" href="#home">Inicio</a></li>
-									<li><a class="menu" href="#buscar">Buscar</a></li>
-									<li><a class="menu" href="#nosotros">Sobre nosotros</a></li>
-									<li><a class="menu" href="#profesor">Profesores</a></li>
-									<li><a class="menu" href="#corrector">¿Quieres ser corrector? </a></li>
-					        <?php if(!$logged){?><li><a class="menu"
-										href="http://ciae.edocere.com/login/index.php">Entrar</a></li><?php }?>
-					        
-					        
-					      </ul>
-							</div>
-							<!-- /navbar-collapse -->
-						</div>
-						<!-- / .container-fluid -->
-					</nav>
-				</div>
+<section class="barraResultado">
+<hr>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-1">
 			</div>
-		</div>
-	</header>
-	<!-- end of header area -->
-
-
-	<!-- buscar section -->
-	<section class="about text-center" id="buscar">
-		<div class="container">
-			<div class="row">
-				<h2></h2>
-				<div class="col-md-2 col-sm-6"></div>
-				<div class="col-md-10 col-sm-6">
-					<form method="get" action="resultados.php" class="pure-form"
-						style="border-top: 1px solid #eee; border-bottom: 1px solid #eee; background: #fafafa; padding: 20px 10px; text-align: center">
-						<div class="single-about-detail clearfix">
-							<div class="">
-								<div class="form-group">
-
-									<div class="col-xs-7">
-										<input id="recursos" autofocus type="text" name="name"
-											placeholder="Nombre" style="width: 100%; max-width: 600px">
-									</div>
-									<div class="col-xs-2">
-										<button type="submit" class="btn btn-default">Buscar</button>
-
-									</div>
-								</div>
-							</div>
-						</div>
-					</form>
-
-				</div>
+			<div class="col-md-8">
+				<p>3 Resultado(s) </p>
+				
 			</div>
-			</div></section>
-<section>
-			<div class="container">
-    <div class="row clearfix">
-		<div class="col-md-12 column">
-			<table class="table table-bordered table-hover" id="tab_logic">
-				<thead>
-					<tr >
-						<th class="text-center">
-							#
-						</th>
-						<th class="text-center">
-							Name
-						</th>
-						<th class="text-center">
-							Mail
-						</th>
-						<th class="text-center">
-							Mobile
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr id='addr0'>
-						<td>
-						1
-						</td>
-						<td>
-						<input type="text" name='name0'  placeholder='Name' class="form-control"/>
-						</td>
-						<td>
-						<input type="text" name='mail0' placeholder='Mail' class="form-control"/>
-						</td>
-						<td>
-						<input type="text" name='mobile0' placeholder='Mobile' class="form-control"/>
-						</td>
-					</tr>
-                    <tr id='addr1'></tr>
-				</tbody>
-			</table>
+			<div class="col-md-2">
+				<select class="form-control">
+					<option>Ordenar por:</option>
+ 					<option>Comentarios</option>
+  					<option>Likes</option>
+  					<option>Vistos</option>
+				</select>
+			</div>
 		</div>
 	</div>
-	<a id="add_row" class="btn btn-default pull-left" onclick="myFunction()">Add Row</a><a id='delete_row' class="pull-right btn btn-default">Delete Row</a>
-</div>
-			
-			
-			
 </section>
-	<!-- end of buscar section -->
-
-	<!-- footer starts here -->
-	<footer class="footer clearfix">
-		<div class="container">
-			<div class="row">
-				<div class="col-xs-6 footer-para">
-					<p>&copy; All right reserved</p>
-
-				</div>
-				<div class="col-xs-6 text-right">
-					<a href=""><i class="fa fa-facebook"></i></a> <a href=""><i
-						class="fa fa-twitter"></i></a>
+<section class="resultados">
+	<div class="container">
+		<div id="filtros" class="col-md-3" style="text-align: left">
+			<div class="panel panel-default">
+				<div class="panel-body">	
+					<h1 style="text-align: left">Filtros</h1>
+					<h3 style="text-align: left">Tipo de recurso</h3>
+					<ul>
+						<li>
+						<div class="checkbox">
+						<input type="checkbox" name="rubrica" value="1"> Actividad
+						</div>
+						</li>
+						<li>
+						<div class="checkbox">
+						<input type="checkbox" name="rubrica" value="1"> Rúbrica
+						</div>
+						</li>
+					</ul>
+					<h3 style="text-align: left">Géneros</h3>
+					<ul id="genero">
+						<?php
+						// recorre el arreglo que contiene todos los generos, por cada uno crea un checkbox y una lista
+						for($i=0;$i<count($generos);$i++){
+							echo'<li>
+							<div class="checkbox">
+							<input type="checkbox" name="'.$generos[$i].'"> '.$generos[$i].'
+							</div>
+							</li>';
+						}
+						?>
+					</ul>
+					<div id="loadMore">Ver más...</div>
+					<div id="showLess">Ver menos...</div>	
+					<h3 style="text-align: left"></h3>
+					
+					<h3 style="text-align: left">Calificación</h3>
+					<ul id="calificacion">
+						<li>
+						<div class="checkbox">
+							<input type="checkbox" name="unaEstrella" value="1">
+							<span class="glyphicon glyphicon-star" aria-hidden="true" ></span>
+							<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+							<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+							<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+							<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+						</div>
+						</li>
+						<li>
+						<div class="checkbox">
+							<input type="checkbox" name="dosEstrella" value="1">
+							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+							<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+							<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+							<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+						</div>
+						</li>
+						<li>
+						<div class="checkbox">
+							<input type="checkbox" name="tresEstrella" value="1">
+							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+							<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+							<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+						</div>
+						</li>
+						<li>
+						<div class="checkbox">
+							<input type="checkbox" name="cuatroEstrella" value="1">
+							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+							<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+						</div>
+						</li>
+						<li>
+						<div class="checkbox">
+							<input type="checkbox" name="cincoEstrella" value="1">
+							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+							<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+						</div>
+						</li>
+					</ul>					
 				</div>
 			</div>
 		</div>
-	</footer>
-
-
-	<!-- script tags
-	============================================================= -->
-	<script src="js/jquery-2.1.1.js"></script>
-
-	<script src="js/smoothscroll.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/custom.js"></script>
-	<script src="js/auto-complete.js"></script>
-	
-	
-
-<script>
-function myFunction() {
-    var table = document.getElementById("tab_logic");
-    var row = table.insertRow(2);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    cell1.innerHTML = "NEW CELL1";
-    cell2.innerHTML = "NEW CELL2";
-}
-</script>
-</body>
-</html>
+		<div id="resultados" class="col-md-9" style="text-align: left">
+			
+			<div class="panel panel-default">
+			<div class="single-result-detail clearfix">
+			<div data-toggle="modal" data-target="#myModal">
+			
+				<div id="descripcion" class="panel-body">
+			<center><h3>Amigos por correspondencia</h3></center>
+					<div  class="col-md-4" style="text-align: left">
+						<p>Propósito Comunicativo: Informar</p>
+						<p>Género: Carta de presentación personal</p>
+						<p>Audiencia: Amigo</p>
+						<p>Tiempo estimado: 90 min.</p>
+						<p>Contiene: 
+							<span class="glyphicon glyphicon-film" aria-hidden="false"></span>
+							<span class="glyphicon glyphicon-book" aria-hidden="false"></span>
+							<span class="glyphicon glyphicon-print" aria-hidden="false"></span>
+						</p>
+					</div>
+					<div  class="col-md-5">
+						<p>El producto final es una carta
+							de dos párrafos de extensión destinada a un amigo de un
+							curso paralelo, de un colegio cercano o de séptimo año,
+							según resulte más adecuado a la realidad del colegio El
+							producto final es una carta de dos párrafos de extensión
+							destinada a un amigo de un curso paralelo, de un colegio
+							cercano o de séptimo año, según resulte más adecuado a la
+							realidad del colegio</p>
+					</div>
+					<div  class="col-md-3" style="text-align: left">
+					<img src="img/premio.png" class="premio" height="40px" width="40px">
+					<p>55 Visitas</p>
+					<p>3 Comentarios</p>
+					<p>20 votos</p>
+					<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+					<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+					<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+					<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+					<span class="glyphicon glyphicon-star-empty" aria-hidden="true"></span>
+					<p></p><p></p>
+					<p style="font-size: 14px;">Creado por:<a href="perfil.html"> Natalia Ávila</a></p>
+					
+					</div>
+				</div>
+			</div>	
+			</div>
+			</div>
+			
+			
+			</div>	
+			</div>
+			</div>
+			
+		</div>
+	</div>
+</section>
